25a28524bf5975a69ca568cceb156d48ed7c36bb
Luiz <nandoluizprimeiro@gmail.com>
2025-04-01 09:10:08 -0300

    declaracao de variaveis

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// Declaracoes e variaveis
+
+var nome = "Fiap";
+console.log (nome)
+
+let idade = 19;
+console.log (idade)
+
+const sobrenome = "Balbino";
+console.log(sobrenome)
+
+let aula;
+console.log(aula)
+
+let valor = null;
+console.log(valor)
+
+let exemplo={};
+
+let exemplo2=[];
+
+// alert("Morra")
